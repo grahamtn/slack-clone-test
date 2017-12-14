ab1d45f23babd6e1e6ce78a325f61af9b5261f7d
graham <grahamnilsen@gmail.com>
2017-12-14 16:29:14 -0500

    mock messages now have timestamp

--- src/app/mock-messages.ts
+++ src/app/mock-messages.ts
@@ -1,10 +1,10 @@
 import { Message } from './message';
 
 export var MESSAGES: Message[] = [
-    {messageBody: 'Yo Dude!'},
-    {messageBody: 'Whats up Dude!'},
-    {messageBody: 'Nuttin Dude!'},
-    {messageBody: 'Thats cool Dude!'},
-    {messageBody: 'BRB I gotta shit'},
-    {messageBody: 'FUCK YEAH DUDE!'}
+    {messageBody: 'Yo Dude!', timeStamp: "4:20pm"},
+    {messageBody: 'Whats up Dude!', timeStamp: "4:20pm"},
+    {messageBody: 'Nuttin Dude!',timeStamp: "4:20pm"},
+    {messageBody: 'Thats cool Dude!',timeStamp: "4:20pm"},
+    {messageBody: 'BRB I gotta shit',timeStamp: "4:20pm"},
+    {messageBody: 'FUCK YEAH DUDE!',timeStamp: "4:20pm"}
 ];
