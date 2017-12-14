c6fa37ebb9b8165b53f8800e8b38f48ab3bc5aa4
graham <grahamnilsen@gmail.com>
2017-12-13 20:10:06 -0500

    working demo

--- src/app/message-input/message-input.component.ts
+++ src/app/message-input/message-input.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Message } from '../message';
+import { MESSAGES } from '../mock-messages';
 
 @Component({
-  selector: 'app-message-input',
-  templateUrl: './message-input.component.html',
-  styleUrls: ['./message-input.component.css']
+    selector: 'app-message-input',
+    templateUrl: './message-input.component.html',
+    styleUrls: ['./message-input.component.css']
 })
-export class MessageInputComponent implements OnInit {
+export class MessageInputComponent {
 
-  @Input() message: Message;
+    textValue = 'initial value';
+    log = '';
 
-  constructor() { }
+    logText(value: string): void {
+        this.createMessage(this.textValue);
 
-  ngOnInit() {
-  }
-
-
-
+        //this.log += `Text changed to '${value}'\n`;
+    }
+    createMessage(input:String){
+        let message: Message = new Message(input);
+        MESSAGES.push(message);
+        console.log(message.messageBody);
+    }
 }
